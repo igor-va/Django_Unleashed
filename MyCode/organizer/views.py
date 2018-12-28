20bcbad65a33fd6c8c9353369f4e7a4dc16a7893
Igor <igoryan16031991@gmail.com>
2018-12-28 10:18:05 +0300

    Init 2

--- MyCode/organizer/views.py
+++ MyCode/organizer/views.py
@@ -10,3 +10,7 @@ def homepage(request):
     context = {'tag_list': tag_list}
     output = template.render(context)
     return HttpResponse(output)
+
+
+def tag_detail(request):
+    return HttpResponse()

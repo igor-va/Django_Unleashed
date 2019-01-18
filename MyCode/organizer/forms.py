cfc50e1f5bb89d50385d81d066b5f34ede35c3a7
Igor <igoryan16031991@gmail.com>
2019-01-18 18:37:29 +0300

    8. Displaying Forms in Templates

--- MyCode/organizer/forms.py
+++ MyCode/organizer/forms.py
@@ -1,19 +1,55 @@
 from django import forms
-from .models import Tag
+from django.core.exceptions import ValidationError
+
+from .models import NewsLink, Startup, Tag
 
 
-class TagForm(forms.Form):
-    name = forms.CharField(max_length=31)
-    slug = forms.SlugField(
-        max_length=31,
-        help_text='A label for URL config')
-
-    def save(self):
-        new_tag = Tag.objects.create(name = self.cleaned_data['name'],
-                                    slug = self.cleaned_data['slug'])
-        return new_tag
+class NewsLinkForm(forms.ModelForm):
+    class Meta:
+        model = NewsLink
+        fields = '__all__'
 
 
+class SlugCleanMixin:
+    """Mixin class for slug cleaning method."""
+
+    def clean_slug(self):
+        new_slug = (
+            self.cleaned_data['slug'].lower())
+        if new_slug == 'create':
+            raise ValidationError(
+                'Slug may not be "create".')
+        return new_slug
 
 
+class StartupForm(
+        SlugCleanMixin, forms.ModelForm):
+    class Meta:
+        model = Startup
+        fields = '__all__'
 
+    def clean_slug(self):
+        new_slug = (
+            self.cleaned_data['slug'].lower())
+        if new_slug == 'create':
+            raise ValidationError(
+                'Slug may not be "create".')
+        return new_slug
+
+
+class TagForm(
+        SlugCleanMixin, forms.ModelForm):
+    class Meta:
+        model = Tag
+        fields = '__all__'
+
+    def clean_name(self):
+        return self.cleaned_data['name'].lower()
+
+    def clean_slug(self):
+        new_slug = (
+            self.cleaned_data['slug'].lower())
+        if new_slug == 'create':
+            raise ValidationError(
+                'Slug may not be "create".')
+        return new_slug

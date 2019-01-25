599f680aa00ed0a8bd6a159afb280a116c79f167
Igor <igoryan16031991@gmail.com>
2019-01-25 12:00:32 +0300

    9.2.2.3 Replacing tag_create() with TagCreate

--- MyCode/organizer/views.py
+++ MyCode/organizer/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import (
-    get_object_or_404, render)
+    get_object_or_404, redirect, render)
 
+from .forms import TagForm
 from .models import Startup, Tag
 
 
@@ -18,6 +19,20 @@ def startup_list(request):
         request,
         'organizer/startup_list.html',
         {'startup_list': Startup.objects.all()})
+
+
+def tag_create(request):
+    if request.method == 'POST':
+        form = TagForm(request.POST)
+        if form.is_valid():
+            new_tag = form.save()
+            return redirect(new_tag)
+    else:  # request.method != 'POST'
+        form = TagForm()
+    return render(
+        request,
+        'organizer/tag_form.html',
+        {'form': form})
 
 
 def tag_detail(request, slug):

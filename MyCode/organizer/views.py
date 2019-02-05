6f313893799d75a958f45f2a0ab20f1eeb655b1f
Igor <igoryan16031991@gmail.com>
2019-02-05 19:00:29 +0300

    14.5 Pagination of Tag List Webpage Using the URL Path

--- MyCode/organizer/views.py
+++ MyCode/organizer/views.py
@@ -1,3 +1,5 @@
+from django.core.paginator import (
+    EmptyPage, PageNotAnInteger, Paginator)
 from django.urls import reverse_lazy
 from django.shortcuts import (
     get_object_or_404, redirect, render)
@@ -92,11 +94,46 @@ def startup_detail(request, slug):
         {'startup': startup})
 
 
-def startup_list(request):
-    return render(
-        request,
-        'organizer/startup_list.html',
-        {'startup_list': Startup.objects.all()})
+class StartupList(View):
+    page_kwarg = 'page'
+    paginate_by = 5  # 5 items per page
+    template_name = 'organizer/startup_list.html'
+
+    def get(self, request):
+        startups = Startup.objects.all()
+        paginator = Paginator(
+            startups, self.paginate_by)
+        page_number = request.GET.get(
+            self.page_kwarg, 1)
+        try:
+            page = paginator.page(page_number)
+        except PageNotAnInteger:
+            page = paginator.page(1)
+        except EmptyPage:
+            page = paginator.page(
+                paginator.num_pages)
+        if page.has_previous():
+            prev_url = "?{pkw}={n}".format(
+                pkw=self.page_kwarg,
+                n=page.previous_page_number())
+        else:
+            prev_url = None
+        if page.has_next():
+            next_url = "?{pkw}={n}".format(
+                pkw=self.page_kwarg,
+                n=page.next_page_number())
+        else:
+            next_url = None
+        context = {
+            'is_paginated':
+                page.has_other_pages(),
+            'next_page_url': next_url,
+            'paginator': paginator,
+            'previous_page_url': prev_url,
+            'startup_list': page,
+        }
+        return render(
+            request, self.template_name, context)
 
 
 class StartupUpdate(ObjectUpdateMixin, View):

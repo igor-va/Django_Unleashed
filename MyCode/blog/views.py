dc03802dd4c0be8f594f36eb0aaff5fb980e4060
Igor <igoryan16031991@gmail.com>
2019-02-17 00:16:17 +0200

    18. Advanced Generic Class-Based View Usage

--- MyCode/blog/views.py
+++ MyCode/blog/views.py
@@ -2,32 +2,29 @@ from django.shortcuts import (
     get_object_or_404, redirect, render)
 from django.views.decorators.http import \
     require_http_methods
-from django.views.generic import View
+from django.views.generic import (
+    ArchiveIndexView, CreateView,
+    MonthArchiveView, View, YearArchiveView)
 
 from .forms import PostForm
 from .models import Post
 
 
-class PostCreate(View):
+class PostArchiveMonth(MonthArchiveView):
+    model = Post
+    date_field = 'pub_date'
+    month_format = '%m'
+
+
+class PostArchiveYear(YearArchiveView):
+    model = Post
+    date_field = 'pub_date'
+    make_object_list = True
+
+
+class PostCreate(CreateView):
     form_class = PostForm
-    template_name = 'blog/post_form.html'
-
-    def get(self, request):
-        return render(
-            request,
-            self.template_name,
-            {'form': self.form_class()})
-
-    def post(self, request):
-        bound_form = self.form_class(request.POST)
-        if bound_form.is_valid():
-            new_post = bound_form.save()
-            return redirect(new_post)
-        else:
-            return render(
-                request,
-                self.template_name,
-                {'form': bound_form})
+    model = Post
 
 
 class PostDelete(View):
@@ -66,13 +63,15 @@ def post_detail(request, year, month, slug):
         {'post': post})
 
 
-class PostList(View):
-
-    def get(self, request):
-        return render(
-            request,
-            'blog/post_list.html',
-            {'post_list': Post.objects.all()})
+class PostList(ArchiveIndexView):
+    allow_empty = True
+    allow_future = True
+    context_object_name = 'post_list'
+    date_field = 'pub_date'
+    make_object_list = True
+    model = Post
+    paginate_by = 5
+    template_name = 'blog/post_list.html'
 
 
 class PostUpdate(View):

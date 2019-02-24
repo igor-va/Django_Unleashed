825802143b22171c5e01ab21f086d97759f77404
Igor <igoryan16031991@gmail.com>
2019-02-24 23:15:13 +0200

    19.5.4 Post-Authentication Redirection

--- MyCode/suorganizer/urls.py
+++ MyCode/suorganizer/urls.py
@@ -12,6 +12,7 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
+
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import (
@@ -21,6 +22,7 @@ from blog import urls as blog_urls
 from contact import urls as contact_urls
 from organizer.urls import (
     startup as startup_urls, tag as tag_urls)
+from user import urls as user_urls
 
 urlpatterns = [
     url(r'^$',
@@ -36,4 +38,5 @@ urlpatterns = [
     url(r'^contact/', include(contact_urls)),
     url(r'^startup/', include(startup_urls)),
     url(r'^tag/', include(tag_urls)),
+    url(r'^user/', include(user_urls, 'dj-auth'), namespace='dj-auth'),
 ]

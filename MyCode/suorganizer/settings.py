0806ce9ecea771c128350a8e6a90e6d754732c0e
Igor <igoryan16031991@gmail.com>
2019-04-02 19:04:09 +0300

    26.3.5.3 Limiting Fields

--- MyCode/suorganizer/settings.py
+++ MyCode/suorganizer/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.sites',
     'django.contrib.staticfiles',
-    'debug_toolbar',
+    # 'debug_toolbar',
     'django_extensions',
     'core',
     'organizer',
